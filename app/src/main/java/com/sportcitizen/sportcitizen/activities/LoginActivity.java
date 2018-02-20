cd18cac71e7e5673c1316b3fd6e3840dd2030968
Axel Drozdzynski <axeldroz@hotmail.fr>
2018-02-20 17:03:12 +0100

    Authentication facebook is ok

--- app/src/main/java/com/sportcitizen/sportcitizen/activities/LoginActivity.java
+++ app/src/main/java/com/sportcitizen/sportcitizen/activities/LoginActivity.java
@@ -1,15 +1,90 @@
 package com.sportcitizen.sportcitizen.activities;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.sportcitizen.sportcitizen.R;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mDatabaseRef;
+
+    private static final int RC_SIGN_IN = 123;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        runLoginMenu();
     }
+
+    private void runLoginMenu() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                try {
+                    mDatabase = FirebaseDatabase.getInstance();
+                }catch (Exception e) {
+                    Log.d("Exception", e.getMessage());
+                }
+                mDatabaseRef = mDatabase.getReference();
+                DatabaseReference ref = mDatabase.getReference("users").child(user.getUid());
+                ref.child("connected").setValue("OK");
+                Intent intent = new Intent(this, MainActivity.class);
+                this.startActivity(intent);
+            } else {
+
+            }
+        }
+    }
+
+    public void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+
+                    }
+                });
+    }
+
 }
