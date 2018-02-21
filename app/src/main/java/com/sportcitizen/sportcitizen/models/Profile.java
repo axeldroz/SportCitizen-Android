5937cea95530121bc7ab3426e7ac1532c5379e64
Axel Drozdzynski <axeldroz@hotmail.fr>
2018-02-21 14:13:16 +0100

    ProfileView : create base of design

---
+++ app/src/main/java/com/sportcitizen/sportcitizen/models/Profile.java
@@ -0,0 +1,15 @@
+package com.sportcitizen.sportcitizen.models;
+
+/**
+ * Created by axeldroz on 21/02/2018.
+ */
+
+public class Profile {
+    public String bio = "";
+    public String email = "";
+    public String favoriteSport = "";
+    public String name = "";
+    public String photoURL;
+
+    public Profile() {}
+}
