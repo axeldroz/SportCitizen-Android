7f0050074db7e85d719ee1163dceb27e57ee4214
Axel Drozdzynski <axeldroz@hotmail.fr>
2018-02-21 14:15:05 +0100

    model : Profile renamed in User

--- app/src/main/java/com/sportcitizen/sportcitizen/models/User.java
+++ app/src/main/java/com/sportcitizen/sportcitizen/models/User.java
@@ -4,12 +4,12 @@ package com.sportcitizen.sportcitizen.models;
  * Created by axeldroz on 21/02/2018.
  */
 
-public class Profile {
+public class User {
     public String bio = "";
     public String email = "";
     public String favoriteSport = "";
     public String name = "";
     public String photoURL;
 
-    public Profile() {}
+    public User() {}
 }
